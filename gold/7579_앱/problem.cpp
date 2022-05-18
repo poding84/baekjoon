820c00a0f063d6a47a88a6a2df230a661fc918ba
unknown <a42072130@gmail.com>
2022-05-18 15:33:31 +0900

    7579

--- gold/7579_앱/problem.cpp
+++ gold/7579_앱/problem.cpp
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <utility>
 
+using namespace std;
 
 int appNum, totMem;
 
 
 int appMem[110];
 int appCost[110];
+
+int memcost[10010];
+
+int max(int x, int y) {
+	return x>y ? x : y;
+}
+
+void printmemcost() {
+	for(int i=0; i<=100; i++) {
+		printf("%d ", memcost[i]);
+	}
+	printf("\n");
+}
 
 int main() {
 	
@@ -19,8 +34,25 @@ int main() {
 	for(int i=0; i<appNum; i++) {
 		scanf("%d", &appCost[i]);
 	}
+	//printmemcost();
 	
+	for(int i=0; i<appNum; i++) {
+		int mem = appMem[i];
+		int cost = appCost[i];
+		
+		for(int j=10000; j>=cost+1; j--) {
+			memcost[j] = max(memcost[j], memcost[j-cost] + mem);
+		}
+		memcost[cost] = max(memcost[cost], mem);
+		//printmemcost();
+	}
 	
+	for(int i=0; i<=10000; i++) {
+		if (memcost[i] >= totMem) {
+			printf("%d", i);
+			return 0;
+		}
+	}
 	
 	
 	
