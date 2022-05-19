ff5ac4d3f88a2ad5dc51ccc2f67bedfe258d54ba
unknown <a42072130@gmail.com>
2022-05-19 15:07:05 +0900

    12852

---
+++ silver/12852_1로만들기2/problem.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h> 
+#include <vector>
+#include <utility>
+#include <queue>
+
+using namespace std;
+
+int prevv[1000010];
+
+
+int main() {
+	
+	int x;
+	
+	scanf("%d", &x);
+	
+	queue<int> q;
+	
+	q.push(x);
+	
+	while(!q.empty()) {
+		int curr = q.front();
+		
+		if (curr == 1) {
+			break;
+		}
+		
+		
+		q.pop();
+		
+		if ( 1== 1 ){
+			int a = curr - 1;
+			if (prevv[a] == 0) {
+				prevv[a] = curr;
+				q.push(a);
+			}
+		}
+
+		if (curr%2 == 0) {
+			int a = curr/2;
+			if (prevv[a] == 0) {
+				prevv[a] = curr;
+				q.push(a);
+			}
+		}
+		
+		if (curr%3 == 0) {
+			int a = curr/3;
+			if (prevv[a] == 0) {
+				prevv[a] = curr;
+				q.push(a);
+			}
+		}
+		
+	}
+	vector<int> ans;
+	int curr=1;
+	ans.push_back(curr);
+	while(!(curr == x)) {
+		ans.push_back(prevv[curr]);
+		curr = prevv[curr];
+	}
+	
+	printf("%d\n", ans.size()-1);
+	
+	for(int i=ans.size() - 1; i>=0; i--) {
+		printf("%d ", ans[i]);
+	}
+	
+}
