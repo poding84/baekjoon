581b7b0c6676d3af9437f6108d3d06e8686a9360
choi <a42072130@gmail.com>
2022-05-24 22:54:47 +0900

    include.cpp

---
+++ gold/12100_2048/problem.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#include <vector>
+#include <utility>
+#include <queue>
+#include <string.h>
+#include <map>
+
+
+using namespace std;
+
+
+
+int N;
+
+int board[21][21];
+
+int originboard[21][21];
+
+
+int main() {
+	
+	scanf("%d", &N);
+	
+	for(int i=0; i<N; i++) {
+		for(int j=0; j<N; j++) {
+			scanf("%d", &originboard[i][j]);
+		}
+	}
+	
+	 
+}
