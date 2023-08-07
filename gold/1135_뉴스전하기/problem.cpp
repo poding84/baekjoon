424a16f88d940ceb4cd681462930c2ad0d213a1a
poding84 <a42072130@gmail.com>
2023-08-07 22:22:33 +0900

    1135

--- gold/1135_뉴스전하기/problem.cpp
+++ gold/1135_뉴스전하기/problem.cpp
@@ -1,94 +1,63 @@
 #include <stdio.h> 
 #include <vector>
-#include <utility>
-#include <queue>
-#include <math.h>
+#include <algorithm>
 
 
 using namespace std;
 
-vector<pair<int, int> > v;
-int N;
-long long mini;
-void combi(vector<int> s, int r, int index, int depth) {
-	if(r == 0) {
-//		for(int i=0; i<s.size(); i++) {
-//			printf("%d ", s[i]);
-//		}
-//		printf("\n");
-		pair<long long, long long> vec = {0, 0};
-		int idx = 0;
-		for(int i=0; i<v.size(); i++) {
-			if (s[idx] == i) {
-				//printf("+");
-				
-				vec.first += v[i].first;
-				vec.second += v[i].second;
-				
-				idx ++;
-			}
-			else {
-				vec.first -= v[i].first;
-				vec.second -= v[i].second;
-				//printf("-");
-			}
+vector<int> sub[55];
+
+
+int howlong(int curr) {
+
+	if (sub[curr].size() == 0) {
+		return 0;
+	}
+	
+	int temp[55];
+	
+	for(int i=0; i<sub[curr].size(); i++) {
+		temp[i] = (howlong(sub[curr][i]));
+	}
+	
+	sort(temp, temp+sub[curr].size());
+//	printf("%d\n", curr);
+//	for(int i=0; i<sub[curr].size(); i++) {
+//		printf("%d ", temp[i]);
+//	}
+//	printf("--------------\n");
+	int max = -1;
+	
+	for(int i=0; i<sub[curr].size(); i++) {
+		int a = sub[curr].size()-i + temp[i];
+		if (max < a) {
+			max = a;
 		}
-		long long ans = vec.first*vec.first + vec.second*vec.second;
-		if (ans < mini) {
-			mini = ans;
-		}
-		//printf("\n");
-		
-	}
-	else if (depth == N) {
-		return;
-	}
-	
-	else {
-		s[index] = depth;
-        combi(s, r - 1, index + 1, depth + 1);
-        combi(s, r, index, depth + 1);
-		
 	}
 	
 	
-	
-	
-	
+	return max;
 }
 
 
-double solveProblem() {
-	
-	
-	scanf("%d", &N);
-	v.clear();
-	mini = 9e10;
-	
-	
-	for(int i=0; i<N; i++) {
-		int a, b;
-		scanf("%d %d", &a, &b );
-		v.push_back({a, b});
-	}
-	
-	vector<int> s(N/2);
-	combi(s, N/2, 0, 0);
-	
-	
-	return sqrt(double(mini));
-}
 
 int main() {
 	
 	
-	int x;
+	int n; 
 	
-	scanf("%d", &x);
+	scanf("%d", &n);
 	
-	for(int i=0; i<x; i++) {
-		printf("%.10lf\n", solveProblem());
+	for(int i=0; i<n; i++) {
+
+		int t;
+		scanf("%d", &t);
+		if (i==0) {
+			continue;
+		}
+		sub[t].push_back(i);
 	}
+
 	
-	
+	printf("%d", howlong(0));
 }
